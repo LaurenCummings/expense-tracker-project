affcbb061ec76a8211eac49244c942286685079a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-15 23:50:25 -0400

    switched to version 4 of apollo server instead of 5

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { expressMiddleware } from "@as-integrations/express5";
+import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
 import mergedResolvers from "./resolvers/index.js";
