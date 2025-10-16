220cfea0329c2988c202b74c85bcc27032e4b920
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-15 23:57:57 -0400

    added apollo server as plugin

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
 
