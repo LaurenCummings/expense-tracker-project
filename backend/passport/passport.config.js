6646195bd26ab1dbb486e027038771710c60d859
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-18 00:53:41 -0400

    configured passport to check username and password

--- backend/passport/passport.config.js
+++ backend/passport/passport.config.js
@@ -18,5 +18,24 @@ export const configurePassport = async () => {
         } catch (err) {
             done(err);
         }
-    })
+    });
+
+    passport.use(
+        new GraphQLLocalStrategy( async (username, password, done) => {
+            try {
+                const user = await User.findOne({ username });
+                if (!user) {
+                    throw new Error("Invalid username or password");
+                }
+                const validPassword = await bcrypt.compare(password, user.password);
+
+                if (!validPassword) {
+                    throw new Error("Invalid username or password");
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err);
+            }
+        })
+    )
 };
