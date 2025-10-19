0356995438701ee707eb11cd8ca409dca97c0f03
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-19 17:12:14 -0400

    deleted query and typeDef for finding all users - was only for testing purposes

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -68,9 +68,7 @@ const userResolver = {
         }
     },
     Query: {
-        users: (_,_,{ req, res }) => {
-            return users
-        },
+
         user: (_, { userId }) => {
             return users.find((user) => user._id === userId);
         }
