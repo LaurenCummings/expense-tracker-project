c8f50d2d66eb519bb4df1131003181cf35b83177
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-20 21:30:59 -0400

    used context to search for user

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -70,7 +70,7 @@ const userResolver = {
     Query: {
         authUser: async (_, __, context) => {
             try {
-
+                const user = await context.getUser();
             } catch (err) {
                 console.error("Error in authUser: ", err);
                 throw new Error("internal server error");
