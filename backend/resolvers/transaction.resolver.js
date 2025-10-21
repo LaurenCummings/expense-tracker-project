7f7e413580f14962c68e8a0a2d04644d6dae17f8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-20 22:11:50 -0400

    created transactions query

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,18 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-    Query: {},
+    Query: {
+        transactions: async (_, __dirname, context) => {
+            try {
+                if (!context.getUser()) throw new Error("Unauthorized");
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ user: userId });
+            } catch (err) {
+
+            }
+        }
+    },
     Mutation: {},
 };
 
