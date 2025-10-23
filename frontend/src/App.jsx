62f5259b81bdb1956cc0b8aa37fd3cf965496575
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-23 01:04:15 -0400

    added routes for homepage, loginpage, and signup page:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "../src/pages/HomePage";
+import LoginPage from "../src/pages/LoginPage";
+import SignUpPage from "../src/pages/SignUpPage";
 
 function App() {
 
   return (
-    <div className="text-red-500">App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </>
   )
 }
 
