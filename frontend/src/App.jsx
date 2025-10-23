6e4c6b3dafc7e4678486c64d6dd30a7e39d5d2f6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-23 01:08:09 -0400

    added routes for transaction page and not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../src/pages/HomePage";
 import LoginPage from "../src/pages/LoginPage";
 import SignUpPage from "../src/pages/SignUpPage";
+import TransactionPage from "../src/pages/TransactionPage";
+import NotFoundPage from "../src/pages/NotFoundPage";
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   )
