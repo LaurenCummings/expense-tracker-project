22faef497ff90efdfe868c85962ff376907dd97e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-23 00:52:20 -0400

    updated gridBackground import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import GridBackground from "./components/GridBackground.jsx";
+import GridBackground from "./components/ui/GridBackground.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
