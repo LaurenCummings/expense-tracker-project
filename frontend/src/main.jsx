f43f282c7036655c20c406455082db7a8b35af84
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-25 03:14:02 -0400

    added imports for apollo client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
