1ba7a226577f5cdb9db45b72a6bdd6695c268aa7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-25 23:52:39 -0400

    added a comment about our graphql server url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import GridBackground from "./components/ui/GridBackground.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://localhost:4000/graphql", // The URL of our GraphQL server
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them
   credentials: "include", // This tells Apollo Client to send cookies along with every request to the server
 });
