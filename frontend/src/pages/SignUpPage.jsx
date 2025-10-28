6eed89b065026215cd4131756b59adda2f13d092
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-27 23:51:58 -0400

    disabled signup button if loading is true

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -92,7 +92,9 @@ const SignUpPage = () => {
 								<button
 									type='submit'
 									className='w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed'
+									disabled={loading}
 								>
+									{ loading ? "Loading..." : "Sign Up" }
 									Sign Up
 								</button>
 							</div>
