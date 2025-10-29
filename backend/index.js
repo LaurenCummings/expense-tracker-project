a46a04c558fcca99a5cd813418a05c216d7bba8b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-29 00:28:07 -0400

    added path to cookie

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,7 @@ app.use(
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
             secure: false,
             sameSite: 'lax',
+            path: '/',
             httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
         },
         store: store,
