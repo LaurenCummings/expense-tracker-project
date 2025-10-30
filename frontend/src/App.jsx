b6fed1b6b80b8bf4c9a52bb3f16ab60391e78b15
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-30 01:22:40 -0400

    imported navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../src/pages/HomePage";
 import LoginPage from "../src/pages/LoginPage";
 import SignUpPage from "../src/pages/SignUpPage";
