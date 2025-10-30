9f7ef6b8c62094f3cc76f31d7fa94c3ede360705
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-30 01:32:19 -0400

    added return if data is loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,9 @@ function App() {
   console.log("Loading:", loading);
   console.log("Authenticated user:", data);
   console.log("Error:", error);
+
+  if (loading) return null;
+  
   return (
     <>
       { data?.authUser && <Header /> }
