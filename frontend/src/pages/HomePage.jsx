547fbaa50200c332f6b1f5968f360c8756ee2998
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-30 23:59:40 -0400

    fixed toast import

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Cards from "../components/Cards";
 import TransactionForm from "../components/TransactionForm";
 
 import { MdLogout } from "react-icons/md";
-import { toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
