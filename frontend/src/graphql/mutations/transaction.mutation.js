f02f1456d8c177073b71038ac6874718861c7c44
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-01 01:09:17 -0400

    created DeleteTransaction mutation for front end

--- frontend/src/graphql/mutations/transaction.mutation.js
+++ frontend/src/graphql/mutations/transaction.mutation.js
@@ -27,3 +27,17 @@ export const UPDATE_TRANSACTION = gql`
         }
     }
 `
+
+export const DELETE_TRANSACTION = gql`
+    mutation DeleteTransaction($transactionId: ID!) {
+        deleteTransaction(transactionId: $transactionId) {
+            _id
+            description
+            paymentType
+            category
+            amount
+            location
+            date
+        }
+    }
+`
