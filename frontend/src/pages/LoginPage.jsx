c1e1d5972c6fabe3d24d23ca6c01287a6cca52a7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-31 23:54:05 -0400

    disabled login button on loading

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -59,6 +59,7 @@ const LoginPage = () => {
 									className='w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300
 										disabled:opacity-50 disabled:cursor-not-allowed
 									'
+									disabled={loading}
 								>
 									Login
 								</button>
