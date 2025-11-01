64ea99994f4a7f9ac4e0d7644d936e84b38fbb9f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-01 01:03:53 -0400

    created CreateTransaction mutation for front end

---
+++ frontend/src/graphql/mutations/transaction.mutation.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_TRANSACTION = gql`
+    mutation CreateTransaction($input: TransactionInput!) {
+        createTransaction(input: $input) {
+            _id
+            description
+            paymentType
+            category
+            amount
+            location
+            date
+        }
+    }
+`
