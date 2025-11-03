689a19cefd43da96bd68dea48ddf33de0a00c37f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-02 23:26:56 -0500

    imported getTransactions info into cards component

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,6 +1,9 @@
+import { GET_TRANSACTIONS } from "../graphql/queries/transaction.query";
 import Card from "./Card";
+import { GET_TRANSACTIONS } from "../graphql/queries/transaction.query";
 
 const Cards = () => {
+	const { data, loading, error } = useQuery(GET_TRANSACTIONS);
 	return (
 		<div className='w-full px-10 min-h-[40vh]'>
 			<p className='text-5xl font-bold text-center my-10'>History</p>
