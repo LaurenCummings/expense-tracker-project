4ad4bf20e8767c93d9cb55d753d0a1f8aa8388fe
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-04 23:38:52 -0500

    added code to refetch transactions once one has been added

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -3,9 +3,10 @@ import { CREATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 
 const TransactionForm = () => {
-	// TODO => REFETCH ONCE YOU CREATE THE TRANSACTION
 
-	const [createTransaction, { loading, error }] = useMutation(CREATE_TRANSACTION);
+	const [createTransaction, { loading, error }] = useMutation(CREATE_TRANSACTION, {
+		refetchQueries: ["GetTransactions"],
+	});
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
