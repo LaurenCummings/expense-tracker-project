d6754926528c7ee5e251060d30c0cb8fcd611209
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-06 17:50:05 -0500

    deleted console.log out of cards component

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -8,7 +8,6 @@ const Cards = () => {
 	if (error) return <p>Error: {error.message}</p>
 	if (loading) return <p>Loading...</p>
 
-	console.log("cards:", data);
 	return (
 		<div className='w-full px-10 min-h-[40vh]'>
 			<p className='text-5xl font-bold text-center my-10'>History</p>
