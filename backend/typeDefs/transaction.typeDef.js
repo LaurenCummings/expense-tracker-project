4071d0b4036d0c914285fafc7bccf3740a922926
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-09 01:23:39 -0500

    added categoryStastics query

--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -13,7 +13,7 @@ const transactionTypeDef = `#graphql
     type Query {
         transactions: [Transaction!]
         transaction(transactionId:ID!): Transaction
-        # TODO => Add categoryStatistics query
+        categoryStatistics: [CategoryStatistics!]
     }
 
     type Mutation {
