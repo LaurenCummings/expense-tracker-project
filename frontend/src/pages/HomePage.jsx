ac3cb3966b21a6bd4a1c453178a85e23c368f091
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-10 02:41:21 -0500

    created empty array to hold chart border colors

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -42,6 +42,7 @@ const HomePage = () => {
 			const totalAmounts = data.categoryStatistics.map((stat) => stat.totalAmount);
 
 			const backgroundColors = [];
+			const borderColors = [];
 		}
 	}, [data]);
 
