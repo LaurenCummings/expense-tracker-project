8e9434a73062d4d8348186375db34f3b02f81d0e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-13 16:13:09 -0500

    corrected parameter typo -> user should be userId

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -91,7 +91,7 @@ const userResolver = {
     User: {
         transactions:async (parent) => {
             try {
-                const transactions = await Transaction.find({user:parent._id})
+                const transactions = await Transaction.find({userId:parent._id})
             } catch (err) {
                 console.log("Error in user.transactions resolver: ", err);
                 throw new Error(err.message || "Internal server error");
