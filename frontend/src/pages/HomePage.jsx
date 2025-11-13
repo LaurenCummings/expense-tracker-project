80e59da6d6531fbc9b1f3053a7c853e34665d1e2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-12 23:51:18 -0500

    imported getauthenticateduser query into homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ import toast from "react-hot-toast";
 import { useMutation, useQuery } from "@apollo/client/react";
 import { LOGOUT } from "../graphql/mutations/user.mutation";
 import { GET_TRANSACTION_STATISTICS } from "../graphql/queries/transaction.query";
+import { GET_AUTHENTICATED_USER } from "../graphql/queries/user.query";
 import { useEffect, useState } from "react";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
