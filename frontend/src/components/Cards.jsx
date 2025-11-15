c0fd66930905da2f310c6de81c4a2fba7d264671
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-14 23:39:46 -0500

    called getUserAndTransactions query from cards and saved data in constant

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -5,6 +5,7 @@ import { useQuery } from "@apollo/client/react";
 
 const Cards = () => {
 	const { data, loading, error } = useQuery(GET_TRANSACTIONS);
+	const { data:userAndTransactions } = useQuery(GET_USER_AND_TRANSACTIONS);
 
 	if (error) return <p>Error: {error.message}</p>
 	if (loading) return <p>Loading...</p>
