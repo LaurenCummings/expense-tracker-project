4f41ff96d159f2a8afe643e402cb4c19e38ab489
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-19 02:16:52 -0500

    wrote cron job function to send a get request to the url every 14 minutes

--- backend/cron.js
+++ backend/cron.js
@@ -2,3 +2,17 @@ import cron from "cron";
 import https from "https";
 
 const URL = "https://expense-tracker-project-fjc2.onrender.com/"
+
+const job = new cron.CronJob('*/14 * * * *', function() {
+    https.get(URL, (res) => {
+        if (res.statusCode === 200) {
+            console.log("GET request sent successfully")
+        } else {
+            console.log("GET request failed", res.statusCode)
+        }
+    }).on('error', (e) => {
+        console.error("Error while sending request", e);
+    });
+})
+
+export default job;
